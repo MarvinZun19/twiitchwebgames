fa4b62eaa02e79afd39c8df641c34a08a5052d1a
“Marvin <marvin_zuniga19@outlook.com>
2022-11-09 16:58:55 -0600

    second comit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //insert your Twitch API key into this variable for the project to works
-let API_KEY = "n9e2o2yya2xg2ykzxmqv80tsut3ppl";
+let API_KEY = "79zq3jeq2mn89vscgvhvj776536zi5";
 let api = axios.create({
   headers: {
     "Client-ID": API_KEY
